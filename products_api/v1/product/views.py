ca5154c7ada5fe5407ddc260cab0efa140a1d9ae
Yerzhan Tolysbek <45069350+tallisbackatcoding@users.noreply.github.com>
2024-01-24 10:20:54 +0600

    Update views.py

--- products_api/v1/product/views.py
+++ products_api/v1/product/views.py
@@ -21,7 +21,7 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
             filter_args['category'] = self.request.query_params.get('category')
         if self.request.query_params.get('tags'):
             tags = self.request.query_params.get('tags').split(',')
-            filter_args['tags__all'] = tags
+            filter_args['tags__in'] = tags
 
         queryset = queryset.filter(**filter_args)
         search_text = self.request.query_params.get('search')
